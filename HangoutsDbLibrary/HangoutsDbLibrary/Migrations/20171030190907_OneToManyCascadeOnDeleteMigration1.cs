c8a43a750f96267e25268dcb6f83101a9fdae739
Mihai Hornariu <mihai.hornariu@accesa.eu>
2017-11-04 08:53:08 +0200

    .gitignore is working

---
+++ HangoutsDbLibrary/HangoutsDbLibrary/Migrations/20171030190907_OneToManyCascadeOnDeleteMigration1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HangoutsDbLibrary.Migrations
+{
+    public partial class OneToManyCascadeOnDeleteMigration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activity_Groups_GroupNr",
+                table: "Activity");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activity_Groups_GroupNr",
+                table: "Activity",
+                column: "GroupNr",
+                principalTable: "Groups",
+                principalColumn: "GroupNr",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Activity_Groups_GroupNr",
+                table: "Activity");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Activity_Groups_GroupNr",
+                table: "Activity",
+                column: "GroupNr",
+                principalTable: "Groups",
+                principalColumn: "GroupNr",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
---
+++ HangoutsDbLibrary/HangoutsDbLibrary/Model/Group.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HangoutsDbLibrary.Model
+{
+    public class Group
+    {
+        public Group()
+        {
+            UserGroups = new List<UserGroup>();
+        }
+        public string GroupNr { get; set; }
+
+        public Chat Chat { get; set; }
+
+        public GroupAdmin Admin { get; set; }
+
+        public List<Activity> Activities { get; set; }
+
+        public List<UserGroup> UserGroups { get; set; }
+
+        
+    }
+}
